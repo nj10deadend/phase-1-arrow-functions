7563d7fb5fd88226e7f08f6505501925a649136c
nj10deadend <nasjacks10@gmail.com>
2021-07-21 11:26:39 -0400

    commit tree f502e10fbf2e8336cf12356f3b8d4a573d2f4c9b

---
+++ index.js
@@ -0,0 +1,12 @@
+let num1 = 2000;
+let num2 = 100;
+const divide = function () {
+    return num1 / num2;
+}
+
+let x = 2;
+const square = x => x*x;
+
+let a = 3;
+let b = 4;
+const add = (a , b) => a + b; 
